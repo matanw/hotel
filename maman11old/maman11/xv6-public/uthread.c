d307883979722eaa6e1b94a0b42e98ad0c999716
Matan Wiesner <matanwiesner@matanwiesner-macbookpro.roam.corp.google.com>
2019-11-10 22:15:52 +0200

    games in assembly

--- maman11old/maman11/xv6-public/uthread.c
+++ maman11old/maman11/xv6-public/uthread.c
@@ -1,6 +1,21 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int i1;
+int i2;
+int* p1;
+int *p2;
+
+extern void thread_switch(void);
 
 int 
 main(int argc, char *argv[]) 
 {
+    i1=11;
+    p1=*i1;
+    p2= *i2;
+    thread_switch();
+    printf(1, "i2:%d\n",i2);
     return 0;
 }
