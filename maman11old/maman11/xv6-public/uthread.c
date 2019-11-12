d34d3ba1819c12131d897896c96c12c4b10dc789
Matan Wiesner <matanwiesner@matanwiesner-macbookpro.roam.corp.google.com>
2019-11-12 09:34:06 +0200

    print esp and ebp-fix

--- maman11old/maman11/xv6-public/uthread.c
+++ maman11old/maman11/xv6-public/uthread.c
@@ -3,9 +3,9 @@
 #include "user.h"
 
 long int esp_var;
-long int bsp_var;
+long int ebp_var;
 long int* esp_point;
-long int* bsp_point;
+long int* ebp_point;
 
 
 
@@ -17,8 +17,8 @@ main(int argc, char *argv[])
 {
 
     esp_point=&esp_var;
-    bsp_point= &bsp_var;
+    epp_point= &ebp_var;
     thread_switch();
-    printf(1, "bsp:%d , esp:%d\n",bsp_var,esp_var);
+    printf(1, "epp:%d , esp:%d\n",ebp_var,esp_var);
     return 0;
 }
