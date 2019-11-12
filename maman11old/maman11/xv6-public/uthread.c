5499a00f77f434dd816f5b4079fa7431826065a6
Matan Wiesner <matanwiesner@matanwiesner-macbookpro.roam.corp.google.com>
2019-11-12 19:17:05 +0200

    try edit esp

--- maman11old/maman11/xv6-public/uthread.c
+++ maman11old/maman11/xv6-public/uthread.c
@@ -2,21 +2,49 @@
 #include "stat.h"
 #include "user.h"
 
-long int esp_var;
-long int ebp_var;
-long int* esp_point;
-long int* ebp_point;
+#define STACK_SIZE  8192
+#define MAX_THREAD  4
 
-
+typedef struct thread thread_t, *thread_p;
+struct thread {
+    int        sp;                /* curent stack pointer */
+    char stack[STACK_SIZE];       /* the thread's stack */
+    int        state;             /* running, runnable, waiting */
+};
+static thread_t my_thread;
+thread_p  my_thread_p;
 
 
 extern void thread_switch(void);
 
-void print_esp_ebp(){
-
-    thread_switch();
-    printf(1, "ebp:%d , esp:%d - Hex: ebp:%x , esp:%x ",ebp_var,esp_var,ebp_var,esp_var);
+void printNotZeros(){
+    int s=0;
+    for(int i=0;i<STACK_SIZE;i++)
+        if (my_thread.stack[i]!=0)
+            s++;
+    printf(1,"nonzero is %d\n", s);
 }
+
+void
+thread_create(void (*func)())
+{
+    thread_p t=my_thread_p;
+    t->sp = (int) (t->stack + STACK_SIZE);   // set sp to the top of the stack
+    t->sp -= 4;                              // space for return address
+    * (int *) (t->sp) = (int)func;           // push return address on stack
+    t->sp -= 32;
+
+}
+
+
+static void
+afunc(void) {
+    int i;
+    printf(1, "my thread running\n");
+    printNotZeros();
+}
+
+
 int sum(int n){// return sum of 1-> n
 
     printf(1,"call sum, with n= %d\n", n);
@@ -28,14 +56,15 @@ int sum(int n){// return sum of 1-> n
 
 }
 int
-main(int argc, char *argv[]) 
+main(int argc, char *argv[])
 {
-
-    esp_point=&esp_var;
-    ebp_point= &ebp_var;
-    print_esp_ebp();
-    int s=sum(5);
-
-    printf(1,"sum is %d\n", s);
+    my_thread_p=&my_thread;
+    for(int i=0;i<STACK_SIZE;i++)
+        my_thread.stack[i]=0;
+    printNotZeros();
+    thread_create(afunc);
+    printNotZeros();
+    thread_switch();
+    printf(1, "you dont want see me\n");
     return 0;
 }
